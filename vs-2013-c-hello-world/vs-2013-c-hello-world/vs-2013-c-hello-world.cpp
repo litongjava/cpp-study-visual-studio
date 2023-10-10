cccaaee0accafbd2257c2fc9f5a893266fb288c1
litongjava <litongjava@qq.com>
2023-10-09 23:34:58 -1000

    init commit

---
+++ vs-2013-c-hello-world/vs-2013-c-hello-world/vs-2013-c-hello-world.cpp
@@ -0,0 +1,13 @@
+// vs-2013-c-hello-world.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+  printf("vss 2013 c hello world");
+  getchar();
+	return 0;
+}
+
